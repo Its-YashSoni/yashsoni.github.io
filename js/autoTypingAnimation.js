903e551fad3022e3aa64b597c2b9c1d1a1cf664f
Yash Soni <ys00soni36@gmail.com>
2024-01-26 18:26:17 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Yash Soni</span> As a Vice President at SISTec Ratibad and a dedicated BTech student in Artificial Intelligence and Machine Learning (AIML), I bring a diverse skill set in Python programming, Machine Learning, and Flutter development. My passion for technology and leadership experience have equipped me to analyze complex data sets, develop innovative solutions, and manage teams effectively. With a commitment to continuous learning, I am eager to contribute to projects that harness the power of AI and ML, and I welcome connections with like-minded professionals for collaborative opportunities. Let's push the boundaries of technology together. Connect with me to explore transformative solutions."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
